3b76a92786783ff65fc9ca25b162d11d3f546e3d
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-05-03 14:44:38 +0000

    Made a program that displays all numbers from 1000 to 200.

---
+++ five_ints_line.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Sarah
+# Created on: May, 3rd, 2022
+# This program displays numbers from 1000 to 2000 using a for loop. If the
+# number is divisible by 5, it starts a new line.
+
+
+def main():
+    # initialize
+    counter = 0
+
+    # displays the numbers from 1000-2000 & then displays to user.
+    for counter in range(1000, 2001):
+        if counter % 5 == 0 and counter != 100:
+            print("")
+        print(counter, " ", end="")
+
+
+if __name__ == "__main__":
+    main()
